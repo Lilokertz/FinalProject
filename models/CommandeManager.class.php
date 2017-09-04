283c22fe2699f09e38b8ec9f4d958f7b07a776c0
was143 <was143.strasbourg@3wa.fr>
2017-09-04 16:05:56 +0200

    Manager2

---
+++ models/CommandeManager.class.php
@@ -0,0 +1,40 @@
+<?php
+class UserManager
+{
+
+    private $pdo;
+
+    function __construct($pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    public function findById($id)
+    {
+        $sql = "SELECT * FROM commandes WHERE id=?";
+        $query = $this->pdo->prepare($pdo);
+        $query->execute($id);
+        $commande = $query->fetchObject('commande');
+        return $commande;
+    }
+
+    public function create($id_user, $num_commande, $valeur)
+    {
+        $commande = new Commande();
+
+        $commande->setId_user($id_user);
+        $commande->setNum_commande($num_commande);
+        $commande->setValeur($valeur);
+
+        $sql = "INSERT INTO commandes (id_user, num_commande, valeur) VALUES (?, ?, ?)";
+		$query = $this->pdo->prepare($sql);
+		$query->execute([$commande->getId_user(),
+						$commande->getNumCommande(),
+						$commande->getValeur)]);
+		$id = $this->pdo->lastInsertId();
+		return $this->findById($id);
+    }
+
+}
+
+?>
---
+++ models/CommentManager.class.php
@@ -0,0 +1,48 @@
+<?php
+
+    class CommentManager
+    {
+
+        private $pdo;
+
+        function __construct($pdo)
+        {
+            $this->pdo = $pdo;
+        }
+
+        public function findById($id)
+        {
+            $sql = "SELECT * FROM comments WHERE id=?";
+            $query = $this->pdo->prepare($pdo);
+            $query->execute($id);
+            $comments = $query->fetchObject('comments');
+            return $comments;
+        }
+
+        public function create($content, $id_article, $id_author)
+        {
+            $comments = new Comments();
+
+            $comments->setContent($content);
+            $comments->setId_article($article);
+            $comments->setId_author($author);
+
+            $sql = "INSERT INTO comments (content, id_article, id_author) VALUES (?, ?, ?)";
+    		$query = $this->pdo->prepare($sql);
+    		$query->execute([$comments->getContent(),
+    						$comments->getId_article(),
+    						$comments->getId_author)]);
+    		$id = $this->pdo->lastInsertId();
+    		return $this->findById($id);
+        }
+
+        public function remove(Comments $comments)
+        {
+            $sql = "DELETE FROM comments WHERE id=?";
+            $query = $this->pdo->prepare($sql);
+            $query->execute([$comments->getId()]);
+        }
+    }
+
+
+?>
